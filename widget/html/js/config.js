91f434e19bd4eb86bcdd3653c6f0660cb4cd54f6
hengxiao <hengxiao@iflytek.com>
2021-08-11 16:17:10 +0800

    ✨ feat(all): 新增落地页

---
+++ widget/html/js/config.js
@@ -0,0 +1,21 @@
+const androidOptions = {
+  // app协议名
+  scheme: {
+    protocol: 'taobao://'
+  },
+  // app下载地址
+  appstore: 'https://itunes.apple.com/cn/app/id1383186862',
+  // 下载失败跳转地址
+  fallback: '//a.app.qq.com/o/simple.jsp?pkgname=com.youku.shortvideo'
+};
+
+const iphoneOptions = {
+  // app协议名
+  scheme: {
+    protocol: 'taobao://'
+  },
+  // 苹果应用商店
+  appstore: 'https://itunes.apple.com/cn/app/id1383186862',
+  // 打开失败跳转地址
+  fallback: '//a.app.qq.com/o/simple.jsp?pkgname=com.youku.shortvideo'
+};
